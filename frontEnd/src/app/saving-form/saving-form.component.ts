0e8bfb5ccdaf654daaef1943d2416ec85c4d99ea
devMiracle <31959201+tmtonlinestore@users.noreply.github.com>
2018-08-03 11:29:18 +0100

    Update Now

--- frontEnd/src/app/saving-form/saving-form.component.ts
+++ frontEnd/src/app/saving-form/saving-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'angular2-cookie/core'
 
 @Component({
   selector: 'app-saving-form',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saving-form.component.css']
 })
 export class SavingFormComponent implements OnInit {
+  public userId;
+  public token;
 
-  constructor() { }
+  public durations = [
+    {id:1,name:"1 Month"},
+    {id:2,name:"2 Months"},
+    {id:3,name:"3 Months"},
+    {id:4,name:"4 Months"},
+    {id:5,name:"5 Months"},
+    {id:6,name:"6 Months"},
+    {id:7,name:"7 Months"},
+    {id:8,name:"8 Months"},
+    {id:9,name:"9 Months"},
+    {id:10,name:"10 Months"},
+    {id:11,name:"11 Months"},
+    {id:12,name:"12 Months"},
+  ]
+  constructor(private cookieService:CookieService) { }
 
   ngOnInit() {
+    this.userId= this.cookieService.get('payload');
+    // this.userId = payload.user_details._id;
+    // this.token = payload.token;
   }
 
 }
